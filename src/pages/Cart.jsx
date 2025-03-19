15f1b8d4486b5c3f404bac536f98976071d09796
gangahasan <rgangabhavani2@gmail.com>
2025-03-19 12:10:25 +0530

    implemented order summary with dynamic price

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -5,6 +5,19 @@ import "../styles/cart.css"
 
 const Cart = () => {
   const cart = useSelector((state) => state.products.cart) || [];
+
+  let cartprice = cart.reduce((sum, item) => {
+    // Convert price to string then remove any non-digit/non-decimal characters
+    const priceStr = String(item?.price);
+    const numericPrice = Number(priceStr.replace(/[^\d.]/g, ""));
+    return sum + numericPrice * (item?.quantity || 1);
+  }, 0);
+
+
+   // Fixed delivery fee (e.g., 5.00)
+   let deliveryFee = 5.0;
+   let total = cartprice + deliveryFee;
+
   console.log(cart,"from cart page")
   const dispatch = useDispatch();
 
@@ -16,22 +29,39 @@ const Cart = () => {
     <div>
       <h1>Shopping Cart</h1>
       {cart.length > 0 ? (
-        <div className="cart-container">
-          {cart.map((item, index) => (
-            <div key={index} className="cart-item">
+        <div>
+          <div className="cart-container">
+            {cart.map((item, index) => (
+              <div key={index} className="cart-item">
                 <div>
-                    <img src={item.image} alt={item.title} />
+                  <img src={item.image} alt={item.title} />
                 </div>
                 <div>
-                    <h4>{item.title}</h4>
-                    <p>Price: ${item.price}</p>
-                    <p>Rating: {item.rating}</p>
-                </div>    
-              <button onClick={() => handleRemove(item)}>
-                Remove
-              </button>
+                  <h4>{item.title}</h4>
+                  <p>Price: ${item.price}</p>
+                  <p>Rating: {item.rating}</p>
+                </div>
+                <button onClick={() => handleRemove(item)}>Remove</button>
+              </div>
+            ))}
+          </div>
+
+          <div className="order-summary">
+            <h2>Order Summary</h2>
+            <div style={{ display: "flex", flexDirection: "column" }}>
+              <p>
+                Cart price: <span style={{ color: "red" }}>${cartprice}</span>
+              </p>
+              <p>
+                delevery Fee: <span style={{ color: "red" }}>$5</span>
+              </p>
+              <hr />
+              <p>
+                Total Price:<span style={{ color: "red" }}>${total}</span>
+              </p>
             </div>
-          ))}
+            <button>Proceed To Checkout</button>
+          </div>
         </div>
       ) : (
         <p>Your cart is empty.</p>
